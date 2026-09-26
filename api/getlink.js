45f49579d38b4fdac46690a24fdd5bbe676316a7
bagasxitofficial <bagasxitofficial@gmail.com>
2026-09-26 08:05:31 +0700

    Update getlink.js

--- api/getlink.js
+++ api/getlink.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const links = [
     'https://move2link.co/d6e38d7',
     'https://move2link.co/74b15b4',
@@ -9,4 +9,4 @@ export default function handler(req, res) {
   ];
   const target = links[Math.floor(Math.random() * links.length)];
   res.status(200).json({ url: target });
-}
+};
